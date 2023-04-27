cbd6ada2ae31462d2e51f5b7bd01e40012e81caf
MatheusMachad00 <matheus_m_machado@hotmail.com>
2023-04-27 14:03:22 -0300

    Feat: Modal to edit an item created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import MainScreen from "./pages/MainScreen/MainScreen";
-import DeleteModal from "./components/DeleteModal/DeleteModal";
+
 function App() {
   return (
     <>
-      {/* <SignupScreen></SignupScreen> */}
-      {/* <MainScreen></MainScreen> */}
-      <DeleteModal></DeleteModal>
+
     </>
   );
 }
