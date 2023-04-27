0702028df9e2b1f08a698d0fe4dfd69c15abf3a1
MatheusMachad00 <matheus_m_machado@hotmail.com>
2023-04-27 14:18:49 -0300

    Feat: routes to login and home screen created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SignupScreen from "./pages/SignupScreen/SignupScreen";
+import MainScreen from "./pages/MainScreen/MainScreen"
 
 function App() {
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignupScreen />} />
+          <Route path="/home" element={<MainScreen />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
